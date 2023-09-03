e7cd721c43648ada16a76174b21830fb87fe3673
MEENACHI-S <130132919+MEENACHI-S@users.noreply.github.com>
2023-09-03 23:37:41 +0530

    Add files via upload

---
+++ placaement trends/updateapp1.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+# Create flask app
+app = Flask(__name__)
+model = pickle.load(open("updatemodel.pkl", "rb"))
+
+
+# prediction function
+def ValuePredictor(to_predict_list):
+	to_predict = np.array(to_predict_list).reshape(1, 15)
+	loaded_model = pickle.load(open("updatemodel.pkl", "rb"))
+	result = loaded_model.predict(to_predict)
+	return result[0]
+@app.route('/')
+def Home():
+    return render_template("homepage.html")
+
+@app.route('/form')
+def form():
+     return render_template("naven2.html")
+
+@app.route("/result", methods = ["POST"])
+def result():
+    print('Request for predict page received')
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result)== 1:
+            prediction ='Placed'
+        else:
+            prediction ='Not placed '
+    return render_template("updateresult.html", prediction_text = prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
